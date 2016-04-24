9ff23d6d80b5b6b7a80d407a77627c1526801c6a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-24 16:17:28 +0100

    added ejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,5 @@
 module.exports = function(grunt){
-
-	// grunt.initConfig({
-	// 	pkg: grunt.file.readJSON('package.json'),
-	// 	mochaTest: {
-	// 		files: ['test/*.js'],
-	// 		options: {
-	// 			reporter: 'spec'
-	// 		}
-	// 	},
-	// 	watch: {
-	// 		files: ['/*.js', 'test/*.js'],
-	// 		tasks: ['mochaTest']
-	// 	}
-	// });
-  //
-	// grunt.loadNpmTasks('grunt-mocha-test');
-	// grunt.loadNpmTasks('grunt-contrib-watch');
-	// grunt.registerTask('default', 'mochaTest');
-
+  
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     webdriver: {
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-app.get('/', function(request, response){
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
   response.send("Hello world")
 });
+
+app.get('/greetings', function(request, response) {
+  response.render('greeting', { name: "Luke" })
+})
 
 server.listen(3000, function() {
   console.log("Server listening on port 3000");
